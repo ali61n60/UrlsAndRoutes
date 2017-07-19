c56d4616f0f84a9184b9ecf083a8afa85770ef34
ali2 <ali62n62@yahoo.com>
2017-07-19 13:53:18 +0430

    Accessing Route segments via RouteData Object

--- UrlAndRoutes/Controllers/HomeController.cs
+++ UrlAndRoutes/Controllers/HomeController.cs
@@ -9,14 +9,26 @@ namespace UrlsAndRoutes.Controllers
 {
     public class HomeController:Controller
     {
-        public ViewResult Index()
+        public ViewResult Index(string id)
         {
             Result result = new Result
             {
-                Controller = nameof(HomeController),
+                Controller = nameof(HomeController)
+                ,
                 Action = nameof(Index)
             };
             return View("Result",result);
+        }
+
+        public ViewResult CustomVariable()
+        {
+            Result r = new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(CustomVariable),
+            };
+            r.Data["id"] = RouteData.Values["id"];
+            return View("Result", r);
         }
     }
 }
